c90afc6284b486427d21395f560586b7a2f29fa2
ahtziriHuerta <ahtziri.huerta0219@alumnos.udg.mx>
2025-05-13 23:50:01 -0600

    Archivos estáticos colectados

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -88,6 +88,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'tasks' / 'static',
+]
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
