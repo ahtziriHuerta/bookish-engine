9aca9d2b6dac6c3c42234102b71c82b09c7cea90
ahtziriHuerta <ahtziri.huerta0219@alumnos.udg.mx>
2025-05-13 19:36:04 -0600

    Eliminar render.yaml para forzar uso de Procfile

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -73,7 +73,7 @@ urlpatterns = [
     path('tasks/', views.tasks, name='tasks'),
     path('tasks/create/', views.create_task, name='create_task'),
     
-
+    path('tasks/create/', views.create_task, name='create_task'),
     # Página de Inicio
     path('', views.home, name='home'),
 
